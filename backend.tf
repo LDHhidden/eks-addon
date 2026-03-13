9ca3acb633aeb3f7724bacdc60c68d8595cf4369
LDHhidden <ekgudsl5434@naver.com>
2026-03-13 18:25:02 +0900

    key-update2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "eks-addon-state-bucket"
-    key          = "alb_controller/terraform.tfstate"
+    key          = "env/dev/alb_controller/terraform.tfstate"
     region       = "ap-northeast-2"
     use_lockfile = true
   }
